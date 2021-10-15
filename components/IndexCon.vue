052826d5f954ea369498653556b5a7eda2e1f842
FunBuffy <eamongreen59@gmail.com>
2021-10-16 03:15:40 +1300

    Some fixes and addons

---
+++ components/IndexCon.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <!-- Banner -->
+      <section id="banner">
+        <div class="inner">
+          <div class="logo">
+            <img src="images/Q.svg" alt="Q" style="width: 4%; fill: white" />
+          </div>
+          <h2>Welcome to AdquateZone</h2>
+          <p>
+            and yes I do know that it is spelt wrong<i
+              class="fas fa-level-up-alt"
+            ></i>
+          </p>
+        </div>
+      </section>
+
+      <!-- Wrapper -->
+      <section id="wrapper">
+        <!-- One -->
+        <section id="one" class="wrapper spotlight style1">
+          <div class="inner">
+            <a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
+            <div class="content">
+              <h2 class="major">3D Printing</h2>
+              <p>
+                Here you will find all my 3D printing escapades through using a
+                3D printer:<br />
+                I have a Creality resin LCD printer I aquiered off
+                <a href="https://kiwi3d.co.nz">Kiwi3D</a>.<br />More information
+                can be found in the <a href="sSheet.html">Equipment</a> spec
+                sheet on this website.
+              </p>
+              <a href="#" class="special">See more</a>
+            </div>
+          </div>
+        </section>
+
+        <!-- Two -->
+        <section id="two" class="wrapper alt spotlight style2">
+          <div class="inner">
+            <a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
+            <div class="content">
+              <h2 class="major">Design Work</h2>
+              <p>
+                This is where I will have all my: 
+				<ul>
+					<li>3D modeling and deisgns from <a href="www.blender.org">Blender</a></li>
+					<li>Art work from 3 years of design in highschool on Photoshop and Illustrator. <i style="opacity: 0.2;">{Torture}</i></li>
+				</ul>
+              </p>
+              <a href="#" class="special">See more</a>
+            </div>
+          </div>
+        </section>
+
+        <!-- Three -->
+        <section id="three" class="wrapper spotlight style3">
+          <div class="inner">
+            <a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
+            <div class="content">
+              <h2 class="major">Electrician Log</h2>
+              <p>
+                See if this works out but wanna be an electrician,<br> if that doesnt pan out it will be filled with practical electronics and circuit projects.
+              </p>
+              <a href="#" class="special">See more</a>
+            </div>
+          </div>
+        </section>
+      </section>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <IndexCon/>
+</template>
+
+<script>
+export default {}
+</script>
