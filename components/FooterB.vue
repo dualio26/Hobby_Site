68d720e9cc904dd4cc9e8e55a19b17019dd8185a
dualio26 <eamongreen59@gmail.com>
2021-10-30 23:29:25 +1300

    I have been here for 12 hours

--- components/FooterB.vue
+++ components/FooterB.vue
@@ -32,7 +32,7 @@
             </li>
             <li class="icon brands fa-discord">
               <a href="https://discordapp.com/users/AdequateLumps🍑#7244/"
-                >AdequateLumps🍑#7244asdlkfjl̰ɛ᷆s᷅djgl</a
+                >AdequateLumps🍑#7244</a
               >
             </li>
             <li class="icon brands fa-reddit">
